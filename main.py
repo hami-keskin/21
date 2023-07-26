630a693da4a527b0735fb796002ff32264616784
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-26 12:29:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import random
 
 
+class Player:
+    def __init__(self):
+        self.chips = 100  # Initial chips
+        self.wins = 0
+        self.losses = 0
+        self.draws = 0
+
+    def win_chips(self, amount):
+        self.chips += amount
+        self.wins += 1
+
+    def lose_chips(self, amount):
+        self.chips -= amount
+        self.losses += 1
+
+    def draw_chips(self):
+        self.draws += 1
+
+    def __str__(self):
+        return f"Chips: {self.chips}, Wins: {self.wins}, Losses: {self.losses}, Draws: {self.draws}"
+
+
 def create_deck(num_decks):
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     deck = cards * num_decks
@@ -8,7 +30,10 @@ def create_deck(num_decks):
     return deck
 
 
-def deal_card(deck):
+def deal_card(deck, num_decks):
+    if not deck:
+        print("Deck is empty. Reshuffling...")
+        deck = create_deck(num_decks)  # Reshuffle the deck if empty
     return deck.pop()
 
 
@@ -32,19 +57,19 @@ def count_high_cards(deck):
 
 def compare(user_score, computer_score):
     if user_score == computer_score:
-        return "It's a draw!"
+        return "Draw"
     elif computer_score == 0:
-        return "You lose, opponent has Blackjack!"
+        return "Loss"
     elif user_score == 0:
-        return "You win with a Blackjack!"
+        return "Win"
     elif user_score > 21:
-        return "You went over. You lose!"
+        return "Loss"
     elif computer_score > 21:
-        return "Opponent went over. You win!"
+        return "Win"
     elif user_score > computer_score:
-        return "You win!"
+        return "Win"
     else:
-        return "You lose!"
+        return "Loss"
 
 
 def suggest_action(user_score, computer_card, deck):
@@ -75,42 +100,60 @@ def play_game():
     num_decks = 4
     deck = create_deck(num_decks)
 
-    user_cards = []
-    computer_cards = []
-    is_game_over = False
+    user = Player()
 
-    for _ in range(2):
-        user_cards.append(deal_card(deck))
-        computer_cards.append(deal_card(deck))
+    while True:
+        user_cards = []
+        computer_cards = []
+        is_game_over = False
 
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
+        for _ in range(2):
+            user_cards.append(deal_card(deck, num_decks))
+            computer_cards.append(deal_card(deck, num_decks))
 
-    print(f" Your cards: {user_cards}, current score: {user_score}")
-    print(f" Computer's first card: {computer_cards[0]}")
-    print(suggest_action(user_score, computer_cards[0], deck))  # Pass the entire deck
-
-    while not is_game_over:
-        should_deal = input("Type 'y' to get another card, or 'n' to pass: ").lower()
-
-        if should_deal == 'y':
-            user_cards.append(deal_card(deck))
-            user_score = calculate_score(user_cards)
-            print(f" Your cards: {user_cards}, current score: {user_score}")
-            print(suggest_action(user_score, computer_cards[0], deck))  # Pass the entire deck
-            if user_score > 21:
-                is_game_over = True
-        else:
-            is_game_over = True
-
-    while computer_score != 0 and computer_score < 17:
-        computer_cards.append(deal_card(deck))
+        user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
 
-    print(f" Your final hand: {user_cards}, final score: {user_score}")
-    print(f" Computer's final hand: {computer_cards}, final score: {computer_score}")
-    print(compare(user_score, computer_score))
+        print(f" Your cards: {user_cards}, current score: {user_score}")
+        print(f" Computer's first card: {computer_cards[0]}")
+        print(suggest_action(user_score, computer_cards[0], deck))  # Pass the entire deck
+
+        while not is_game_over:
+            should_deal = input("Type 'y' to get another card, or 'n' to pass: ").lower()
+
+            if should_deal == 'y':
+                user_cards.append(deal_card(deck, num_decks))
+                user_score = calculate_score(user_cards)
+                print(f" Your cards: {user_cards}, current score: {user_score}")
+                print(suggest_action(user_score, computer_cards[0], deck))  # Pass the entire deck
+                if user_score > 21:
+                    is_game_over = True
+            else:
+                is_game_over = True
+
+        while computer_score != 0 and computer_score < 17:
+            computer_cards.append(deal_card(deck, num_decks))
+            computer_score = calculate_score(computer_cards)
+
+        print(f" Your final hand: {user_cards}, final score: {user_score}")
+        print(f" Computer's final hand: {computer_cards}, final score: {computer_score}")
+
+        result = compare(user_score, computer_score)
+        if result == "Win":
+            user.win_chips(10)  # You can adjust the winning chips amount as desired
+        elif result == "Loss":
+            user.lose_chips(10)  # You can adjust the losing chips amount as desired
+        else:
+            user.draw_chips()
+
+        print(f"Result: {result}")
+        print(user)
+
+        play_again = input("Do you want to play another round? (y/n): ").lower()
+        if play_again != 'y':
+            print("Thanks for playing!")
+            break
 
 
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
+if __name__ == "__main__":
     play_game()
